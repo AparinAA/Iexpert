af12dfaf49aced6a81d2c2888920b359d0c9415f
AparinAA <alxaparin@gmail.com>
2020-07-01 22:28:45 +0300

    test

--- expert/urls.py
+++ expert/urls.py
@@ -31,6 +31,7 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)),
@@ -71,4 +72,3 @@ urlpatterns += [
 
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
