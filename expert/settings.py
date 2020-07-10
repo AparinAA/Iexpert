4ce43b6d5345f2f6cf1ae1d2a1510ffc9c06cc46
AparinAA <alxaparin@gmail.com>
2020-07-10 18:17:27 +0300

    test upgrade

--- expert/settings.py
+++ expert/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from urllib.parse import urlparse
-
+#TEST UPGRADE
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','expert.settings.production')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
